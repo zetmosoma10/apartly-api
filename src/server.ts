e361dc38b2c817e3fdc964dbbcecf7fc1f5c712e
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-09-29 22:41:07 +0200

    connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("Helllo");
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+import { env } from "process";
+import mongoose from "mongoose";
+
+async function main() {
+  const dbUrl = env.APARTLY_DATABASE_URL;
+  if (!dbUrl)
+    throw new Error("❌ Missing environment variable: APARTLY_DATABASE_URL");
+
+  const port = env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
+  });
+
+  await mongoose.connect(dbUrl);
+  console.log("Successfully connected to database...");
+}
+
+main().catch((err) => {
+  console.log("App FAILED to run, ", err);
+  process.exit(1);
+});
